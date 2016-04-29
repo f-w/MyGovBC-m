2aa8b9694d4e8579bc4befb7508417f6aed86b86
f-w <fred.wen@gov.bc.ca>
2016-04-29 15:43:16 -0700

    added barcode scanner; run ionic add platform android.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,27 @@
 angular.module('app.controllers', [])
-  
-.controller('homeCtrl', function($scope) {
 
+.controller('homeCtrl', function($scope, $cordovaBarcodeScanner) {
+  $scope.scan = function(){
+    document.addEventListener("deviceready", function () {
+      $cordovaBarcodeScanner
+        .scan()
+        .then(function(barcodeData) {
+          // Success! Barcode data is here
+          alert(barcodeData.text)
+        }, function(error) {
+          // An error occurred
+        })
+    }, false)
+  }
 })
-   
+
 .controller('myServicesCtrl', function($scope) {
-
 })
-   
+
 .controller('discoverCtrl', function($scope) {
 
 })
-   
+
 .controller('settingsCtrl', function($scope) {
 
 })
-    
